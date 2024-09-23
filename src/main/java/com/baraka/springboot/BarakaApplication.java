95630b67db64b1180242114665af05d5899e00a9
XIZZA <emmanuelbaraka27@gmail.com>
2024-09-23 15:47:55 +0300

    Current Work

--- src/main/java/com/baraka/springboot/BarakaApplication.java
+++ src/main/java/com/baraka/springboot/BarakaApplication.java
@@ -15,20 +15,7 @@ public class BarakaApplication implements CommandLineRunner {
 
 		@Override
 		public void run(String... args) throws Exception {
-			Employee employee = new Employee();
-			// ... rest of the code to create and save employees
-			employee.setEmployee_name("Jaba Mangi");
-			employee.setEmail("jmangi@gmail.com");
-			employee.setSalary(65555);
-			employee.setAge(88);
-			employeeRepository.save(employee);
 
-			Employee employee1 = new Employee();
-			employee1.setEmployee_name("Blue Band");
-			employee1.setEmail("bband@gmail.com");
-			employee1.setSalary(623222);
-			employee1.setAge(56);
-			employeeRepository.save(employee1);
 		}
 
 		public static void main(String[] args) {
