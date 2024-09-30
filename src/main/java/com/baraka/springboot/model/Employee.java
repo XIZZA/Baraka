eaf402c64231fa521fa2818918a2d3ea88503572
XIZZA <emmanuelbaraka27@gmail.com>
2024-09-30 11:28:51 +0300

    Current Work

--- src/main/java/com/baraka/springboot/model/Employee.java
+++ src/main/java/com/baraka/springboot/model/Employee.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.regex.Pattern;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,16 +20,31 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
 
-    @Column(name = "employee_name")
+    @Column(name = "employee_name", length = 30, nullable = false)
     private String employee_name;
 
-    @Column(name = "email")
+    @Column(name = "email", length = 20, nullable = false)
     private String email;
 
-    @Column(name = "salary")
+    @Column(name = "salary", nullable = false)
     private int salary;
 
-    @Column(name = "age")
+    @Column(name = "age", nullable = false)
     private int age;
 
+    @PrePersist
+    protected void onCreate() {
+        if (salary > 999999999) {
+            throw new IllegalArgumentException("Salary cannot be more than 9 integers");
+        }
+        if (age < 18 || age > 70) {
+            throw new IllegalArgumentException("Age must be between 18 and 70");
+        }
+
+        // Validate email using a regular expression
+        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$";
+        if (!Pattern.matches(emailRegex, email)) {
+            throw new IllegalArgumentException("Invalid email format");
+        }
+    }
 }
